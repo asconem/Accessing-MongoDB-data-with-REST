d58a48165a52d858965b3c408d7daa786f03267c
Matthew Ascone <asconem@gmail.com>
2020-07-05 21:15:26 -0400

    Add PersonRepository interface

--- src/main/java/com/example/accessingmongodbdatarest/AccessingMongoDataRestApplication.java
+++ src/main/java/com/example/accessingmongodbdatarest/AccessingMongoDataRestApplication.java
@@ -1,4 +1,4 @@
-package com.example.accessingmongodatarest;
+package com.example.accessingmongodbdatarest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/accessingmongodbdatarest/AccessingMongoDataRestApplicationTests.java
+++ src/test/java/com/example/accessingmongodbdatarest/AccessingMongoDataRestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.accessingmongodatarest;
+package com.example.accessingmongodbdatarest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
